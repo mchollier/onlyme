ecadee53c8aeb9f31cad249e33ca3f03fe55e1d7
mchollier <marc.chollier@free.fr>
2019-03-12 05:47:34 +0100

    Add Projects

---
+++ PEC/sh/Add_scénario.sh
@@ -0,0 +1,44 @@
+#-----------------------------------------
+# /home/mchollier/sh/Add_scénario.sh ./
+#-----------------------------------------
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_i_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_o_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fin_logi.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fin_logo.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_xdc_sysout.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fi_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fi_fichier-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fi_fichier-003.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_ti_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_fo_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_to_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_to_table-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0001_JCL1_to_table-003.txt
+
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_i_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_o_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fin_logi.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fin_logo.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_xdc_sysout.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fi_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fi_fichier-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fi_fichier-003.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_ti_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_fo_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_to_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_to_table-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0002_JCL1_to_table-003.txt
+
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_i_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_o_log.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fin_logi.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fin_logo.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_xdc_sysout.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fi_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fi_fichier-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fi_fichier-003.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_ti_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_fo_fichier-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_to_table-001.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_to_table-002.txt
+cp TEST_0000_EXEMPLE_i_log.txt TEST_0003_JCL1_to_table-003.txt
